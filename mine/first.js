69c1c182ed5f59fdef193103bc0a4a2d1c5bedaf
xquisitesage <110462982+xquisitesage@users.noreply.github.com>
2022-09-02 14:48:08 +0100

    Add files via upload

---
+++ mine/first.js
@@ -0,0 +1,7 @@
+/* ---------------------------- typing animation ----------------------------- */
+var typed = new Typed(".typing",{
+    strings:["Programmer","Web Designer", "Web Developer", "Teacher", "Public Speaker", "Learned Economist"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
